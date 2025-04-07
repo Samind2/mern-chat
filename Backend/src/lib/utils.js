3b2889ad011f2fd9c6d92a6f3db898d70c34bb75
Samind2 <suphaneerangsirat@gmail.com>
2025-04-07 19:27:51 +0700

    up-production

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -18,6 +18,6 @@ export const generateToken = (userId, res) => {
     // CSRF
     sameSite: "strict",
     // https ?
-    secure: NODE_MODE !== "development",
+    secure: NODE_MODE !== "production", 
   });
 };
